aeb4162bfc0ce1f86601d36e3f064ffaf62a9f2a
Siphamandla brian Mjoli <g14m1190@campus.ru.ac.za>
2019-08-23 02:17:29 +0200

    update
    -Added Camera Shake

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,15 +1,20 @@
-﻿using UnityEngine;
+﻿using EZCameraShake;
+using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
     // Start is called before the first frame update
     public float Speed = 10;
-    public Vector2 moveVector;
+    public Vector2 MoveVector;
     public float MinX, MaxX, MinY, MaxY;
+    public float SmashHit = 15;
+    private int LastPressed = -1; //0 left 1 right up 2 down 3
+    CameraShaker shaker;
+    public float magn = 1000, rough = 500, fadeIn = 1f, fadeOut = 2f;
 
     private void Start()
     {
-
+        shaker = Camera.main.GetComponent<CameraShaker>();
     }
 
     // Update is called once per frame
@@ -22,35 +27,66 @@ public class PlayerMovement : MonoBehaviour
     {
         if (Input.GetKey(KeyCode.LeftArrow))
         {
-            moveVector = Vector2.MoveTowards(transform.position, new Vector2(transform.position.x - 1, transform.position.y), Time.deltaTime * Speed);
-            if(moveVector.x >= MinX)
+            MoveVector = Vector2.MoveTowards(transform.position, new Vector2(transform.position.x - 1, transform.position.y), Time.deltaTime * Speed);
+            if (MoveVector.x >= MinX)
             {
-                transform.position = moveVector;
+                transform.position = MoveVector;
+                LastPressed = 0;
             }
         }
         if (Input.GetKey(KeyCode.RightArrow))
         {
-            moveVector = Vector2.MoveTowards(transform.position, new Vector2(transform.position.x + 1, transform.position.y), Time.deltaTime * Speed);
-            if (moveVector.x <= MaxX)
+            MoveVector = Vector2.MoveTowards(transform.position, new Vector2(transform.position.x + 1, transform.position.y), Time.deltaTime * Speed);
+            if (MoveVector.x <= MaxX)
             {
-                transform.position = moveVector;
+                transform.position = MoveVector;
+                LastPressed = 1;
             }
         }
-        if (Input.GetKey(KeyCode.UpArrow))
+         if (Input.GetKey(KeyCode.UpArrow))
         {
-            moveVector = Vector2.MoveTowards(transform.position, new Vector2(transform.position.x, transform.position.y + 1), Time.deltaTime * Speed);
-            if (moveVector.y <= MaxY)
+            MoveVector = Vector2.MoveTowards(transform.position, new Vector2(transform.position.x, transform.position.y + 1), Time.deltaTime * Speed);
+            if (MoveVector.y <= MaxY)
             {
-                transform.position = moveVector;
+                transform.position = MoveVector;
+                LastPressed = 2;
             }
         }
-        if (Input.GetKey(KeyCode.DownArrow))
+         if (Input.GetKey(KeyCode.DownArrow))
         {
-            moveVector = Vector2.MoveTowards(transform.position, new Vector2(transform.position.x, transform.position.y - 1), Time.deltaTime * Speed);
-            if (moveVector.y >= MinY)
+            MoveVector = Vector2.MoveTowards(transform.position, new Vector2(transform.position.x, transform.position.y - 1), Time.deltaTime * Speed);
+            if (MoveVector.y >= MinY)
             {
-                transform.position = moveVector;
+                transform.position = MoveVector;
+                LastPressed = 3;
             }
         }
+         if (Input.GetKey(KeyCode.Space))
+        {
+            switch (LastPressed)
+            {
+                case 0:
+                    MoveVector = Vector2.MoveTowards(transform.position, new Vector2(transform.position.x - SmashHit, transform.position.y), Time.deltaTime * SmashHit);
+                    break;
+                case 1:
+                    MoveVector = Vector2.MoveTowards(transform.position, new Vector2(transform.position.x + SmashHit, transform.position.y), Time.deltaTime * SmashHit);
+                    break;
+                case 2:
+                    MoveVector = Vector2.MoveTowards(transform.position, new Vector2(transform.position.x, transform.position.y + SmashHit), Time.deltaTime * SmashHit);
+                    break;
+                case 3:
+                    MoveVector = Vector2.MoveTowards(transform.position, new Vector2(transform.position.x, transform.position.y - SmashHit), Time.deltaTime * SmashHit);
+                    break;
+            }
+            if (MoveVector.y >= MinY && MoveVector.y <= MaxY && MoveVector.x <= MaxX && MoveVector.x >= MinX)
+            {
+                transform.position = MoveVector;
+                shaker.ShakeOnce(magn, rough, fadeIn, fadeOut);
+            }
+            LastPressed = -1;
+            
+        }
+
+
     }
 }
